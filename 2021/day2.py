b2227cf80063bbf5c550ff0d8a1d412e389eee91
Your Name <you@example.com>
2021-12-01 19:00:58 +0100

    Advent of code 2021

---
+++ 2021/day2.py
@@ -0,0 +1,14 @@
+import utils
+
+def part_one(input):
+    return sum([1 for i in range(0, len(input) - 1) if input[i+1] > input[i]])
+    
+
+def part_two(input):
+    return sum([1 for i in range(0, len(input) - 2) if sum(input[i+1:i+4]) > sum(input[i:i+3])])
+
+
+input = utils.read_input_int(2)
+
+print(part_one(input))
+print(part_two(input))
